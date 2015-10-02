273b2d8fbde7c5379a884da88ba645dce87149f4
Hemanth.HM <hemanth.hm@gmail.com>
2015-10-02 12:58:53 +0530

    init0

---
+++ README.md
@@ -0,0 +1,21 @@
+#GandhiSays
+> Simple react-native app to get random quotes of Gandhi on each tap.
+
+
+__Looks like:__
+
+__On anroid:__
+
+![GandhiSaid-android.gif](./GandhiSaid-android.gif)
+
+
+__On ios:__
+
+![GandhiSays-ios.png](./GandhiSays-ios.png)
+
+
+__TODO:__
+
+* Use better/custom fonts
+
+* Use local images.
---
+++ index.ios.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var React = require('react-native');
+var GandhiSaid = require('./quotes.json');
+
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity
+} = React;
+
+var GandhiSays = React.createClass({
+  getInitialState: function() {
+    return {quote: ''};
+  },
+  componentWillMount: function() {
+    this._setQuote();
+  },
+  render: function() {
+    return (
+      <View style={styles.container} onTouchStart={this._setQuote}>
+        <Text style={styles.quote}>
+            {this.state.quote}
+        </Text>
+        <Image style={styles.signature} source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Mahatma-Gandhi-Signature-Transparent.png'}}/>
+      </View>
+    );
+  },
+  _setQuote: function() {
+     this.setState({
+      quote: GandhiSaid[Math.floor(Math.random() * GandhiSaid.length)]
+    });
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  quote: {
+    fontSize: 30,
+    textAlign: 'center',
+    margin: 10,
+    color: 'white',
+    fontFamily: 'Cochin'
+  },
+  signature: {
+    height: 30,
+    width: 100
+  }
+});
+
+AppRegistry.registerComponent('GandhiSays', () => GandhiSays);
